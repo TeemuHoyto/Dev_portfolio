3de7064a34bd498e67602b6216db2821720a0d2b
TeemuHoyto <teemu.hoyto@gmail.com>
2019-06-22 08:46:35 +0300

    first commit

---
+++ htdocs/wp-content/themes/summer-theme/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>        
+<head>
+<nav class="top-ar"> <a class="siteTitle" href="<?php echo home_url(); ?>"><?php echo get_bloginfo('name')?></a></nav>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>WPSUMMERPROJECT</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <img class="header_img" id="Iso" src="/wp-content/uploads/2019/06/Header.jpg">
+    </head>
+    <body>
+       
+        <main>
+        <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+        <?php 
+wp_head();
+?>
